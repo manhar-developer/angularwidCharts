e225ba6af6acd3d5c41d200d6e57a2ab6b5465c7
Manhar Sapra <E01561@hsc.com>
2018-04-05 16:51:07 +0530

    changes

--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
+declare var require: any;
+const Highcharts = require('highcharts');
+//Highcharts.setOptions({ lang: { drillUpText: 'Go back'} });
+Highcharts.setOptions({
+  chart: {
+      backgroundColor: {
+          linearGradient: [0, 0, 500, 500],
+          stops: [
+              [0, 'rgb(255, 255, 255)'],
+              [1, 'rgb(240, 240, 255)']
+              ]
+      },
+      borderWidth: 2,
+      plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+      plotShadow: true,
+      plotBorderWidth: 1
+  }
+});
 
 @Component({
   selector: 'app-line-chart',
@@ -26,12 +44,50 @@ export class LineChartComponent implements OnInit {
     },
     series: [{
       name: 'FootFall',
-      data:  [
-        ['Store A', 8],
-        ['Store B', 3],
-        ['Store c', 1],
-        ['Store D', 6]
-    ]
+    //   data:  [
+    //     ['Store A', 80000],
+    //     ['Store B', 3],
+    //     ['Store c', 1],
+    //     ['Store D', 6]
+    // ]
+    data:[
+      {
+          "name": "Chrome44",
+          "y": 62.74,
+          "drilldown": "Chrome"
+      },
+      {
+          "name": "Firefox",
+          "y": 10.57,
+          "drilldown": "Firefox"
+      },
+      {
+          "name": "Internet Explorer",
+          "y": 7.23,
+          "drilldown": "Internet Explorer"
+      },
+      {
+          "name": "Safari",
+          "y": 5.58,
+          "drilldown": "Safari"
+      },
+      {
+          "name": "Edge",
+          "y": 4.02,
+          "drilldown": "Edge"
+      },
+      {
+          "name": "Opera",
+          "y": 1.92,
+          "drilldown": "Opera"
+      },
+      {
+          "name": "Other",
+          "y": 7.62,
+          "drilldown": null
+      }
+  ], 
+  //drilldown: []
     }]
   });
 
