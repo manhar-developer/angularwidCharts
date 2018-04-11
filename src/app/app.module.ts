9745b60401ef8655278939896a7e0de1427bb272
manhar-developer <manharsapra@gmail.com>
2018-04-12 00:03:37 +0530

    Syncronization works finally !!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {ChartModule , HIGHCHARTS_MODULES} from 'angular-highcharts';
 
 import more from 'highcharts/highcharts-more.src';
 import * as exporting from 'highcharts/modules/exporting.src.js';
+import * as funnel from 'highcharts/modules/funnel.src.js';
 
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
@@ -13,7 +14,7 @@ import * as drilldown from 'highcharts/modules/drilldown.src.js';
 
 export function highchartsModules() {
   // apply Highcharts Modules to this array
-  return [ more, exporting, drilldown ];
+  return [ more, exporting, drilldown, funnel ];
 }
 
 @NgModule({
--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -2,34 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 import * as Highcharts from 'highcharts';
 
-//declare var require: any;
-// const Highcharts = require('highcharts');
-
 Highcharts.setOptions({
   chart: {
       backgroundColor: {
-          linearGradient: [0, 0, 500, 500],
+          linearGradient: [0, 0, 0, 50],
           stops: [
               [0, 'rgb(255, 255, 255)'],
               [1, 'rgb(240, 240, 255)']
               ]
       },
-    
+      events: {
+        drilldown: function(e) {
+          const thisIndex = e.point.index;
+  
+          if (!performedDrill) {
+            performedDrill = !performedDrill;
+  
+            Highcharts.charts.forEach(function(chart) {
+              if (e.target !== chart) {
+                chart.get('first-series').points[thisIndex].doDrilldown(false);
+              }
+            });
+          }
+        },
+        drillup: function(e) {
+          if (performedDrill) {
+            performedDrill = !performedDrill;
+            Highcharts.charts.forEach(function(chart) {
+              if (e.target !== chart) {
+                chart.drillUp();
+              }
+            });
+          }
+        }
+      },
       borderWidth: 1,
       plotBackgroundColor: 'rgba(255, 255, 255, .9)',
       plotShadow: true,
-      plotBorderWidth: 20
+      plotBorderWidth: 1
   },
   lang: {},
-  drilldown :{
-    //var clickedEventId = e.seriesOptions.id;
-  },
-    
-  
- 
-});
+  drilldown : {
+  }});
 
-var commonDrilldown = {
+let commonDrilldown = {
   series: [{
     id: 'first',
     colorByPoint: false,
@@ -68,148 +84,119 @@ performedDrill = false;
 
 export class LineChartComponent implements OnInit {
 
-//  xyz(e, point) {
-//    alert("this is xyz");
-//     var pointName = e.point.name;
-//     point.options.chart.drilled = true;
-//     var charts = Highcharts.charts;
-//     Highcharts.each(charts, function(c, i) {
-//       if (!c.options.chart.drilled) {
-//         c.options.chart.drilled = true;
-//         Highcharts.each(c.series[0].data, function(p, j) {
-//           if (p.name === pointName) {
-//             p.doDrilldown();
-//           }
-//         })
-//       }
-//     })
-//   };
 
-
-  constructor() {
-
-     }
-    
- 
+  constructor() {}
   chart = new Chart({
     chart: {
-      type: 'line',
-      events:{drilldown:function(e){
-        //var clickedEventId = e.seriesOptions.id;
-        performedDrill = true;
-      console.log(performedDrill);
-      //console.log(this.plotBorderWidth);
-      }},
+            type: 'bar',
+            zoomType: 'xy',
+          },
+          title: {
+            text: 'Linechart'
+          },
+          plotOptions: {column: {colorByPoint: true}},
+          exporting: {
+            enabled: true,
+           },
+    series: [{
+      id: 'first-series',
+      data: [{
+        y: 3,
+        drilldown: 'first',
+        color: 'rgb(124, 181, 236)'
+      }, {
+        y: 2,
+        drilldown: 'second',
+        color: 'rgb(67, 67, 72)'
+      }, {
+        y: 1,
+        drilldown: 'third',
+        color: '#90ed7d'
+      }]
+    }],
+    drilldown: commonDrilldown
+  });
+
+  chart1 = new Chart({
+    chart: {type: 'column',
+    options3d: {
+                enabled: true,
+                alpha: 45
+            }
+        },
+        title: {
+            text: 'Contents using Pie chart'
+        },
+        subtitle: {
+            text: '3D donut in Highcharts'
+        },
+        plotOptions: {
+            pie: {
+                innerSize: 100,
+                depth: 45}
+        },
+    series: [{
+      id: 'first-series',
+      data: [{
+        name: 'A',
+        y: 3,
+        drilldown: 'first',
+        color:'red'
+      }, {
+        y: 2,
+        drilldown: 'second',
+        color:'green'
+      }, {
+        y: 1,
+        drilldown: 'third',
+        color:'red'
+      }]
+    }],
+    drilldown: commonDrilldown
+  });
+
+  chart2 = new Chart({
+    chart: {
+      type: 'bar',
       zoomType: 'xy',
     },
     title: {
       text: 'Linechart'
     },
     plotOptions: {column: {colorByPoint: true}},
-    exporting: {        
+    exporting: {
       enabled: true,
      },
      series: [{
-      name: 'Operating Systems',
-      data: [
-        {
-          name: 'Windows',
-          y: 88.19,
-          drilldown: 'windows-versions'
-        },
-        {
-          name: 'someOS',
-          x:2.5,
-          y: 80.19,
-         // drilldown: 'windows-versions'
-        },
-        ['MacOSX', 9.22],
-        ['Linux', 1.58],
-        ['Others', 1.01]
-      ]
-    }],
-    drilldown: {
-      series: [{
-        name: 'Windows versions',
-        id: 'windows-versions',
-        data: [
-          ['Win 7', 55.03],
-          ['Win XP', 15.83],
-          ['Win Vista', 3.59],
-          ['Win 8', 7.56],
-          ['Win 8.1', 6.18]
-        ]
+      id: 'first-series',
+      data: [{
+        name: 'A',
+        y: 3,
+        drilldown: 'first'
+      }, {
+        y: 2,
+        drilldown: 'second'
+      }, {
+        y: 1,
+        drilldown: 'third'
       }]
-    }
+    }],
+    drilldown: commonDrilldown
   });
-
-  chart1 = new Chart({
-     chart: {
-      type: 'pie',
-      events:{drilldown:function(e){
-      // console.log(this.chart.clickedEventId);
-      }},
-     
-      options3d: {
-          enabled: true,
-          alpha: 45
-      }
-  },
-  title: {
-      text: 'Contents using Pie chart'
-  },
-  subtitle: {
-      text: '3D donut in Highcharts'
-  },
-  plotOptions: {
-      pie: {
-          innerSize: 100,
-          depth: 45,
-          
-      }
-  },
-series: [{
-  name: 'Operating Systems',
-  data: [
-    {
-      name: 'Windows',
-      y: 88.19,
-      drilldown: 'windows-versions'
-    },
-    ['MacOSX', 9.22],
-    ['Linux', 1.58],
-    ['Others', 1.01]
-  ]
-}],
-drilldown: {
-  series: [{
-    name: 'Windows versions',
-    id: 'windows-versions',
-    data: [
-      ['Win 7', 55.03],
-      ['Win XP', 15.83],
-      ['Win Vista', 3.59],
-      ['Win 8', 7.56],
-      ['Win 8.1', 6.18]
-    ]
-  }]
-}
-});
 
 
 /*
 function to zoom the graph
 */
 zoomContent() {
-  this.chart.ref.yAxis[0].setExtremes(100, 150) ;
+  this.chart.ref.yAxis[0].setExtremes(0, 0.1) ;
 }
 
 /*
 function to rest the zoom in graph
-*/ 
-resetZoom()
-{
-  this.chart.ref.yAxis[0].setExtremes(null,null);
+*/
+resetZoom() {
+  this.chart.ref.yAxis[0].setExtremes(null, null);
 }
 
   ngOnInit() {
